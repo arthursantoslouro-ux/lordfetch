613fb9969d66e7d9793f4bbe0d4ef8e4d7a4dded
arthursantoslouro-ux <arthursantoslouro@gmail.com>
2026-09-11 13:46:08 -0400

    corrigi as flags

--- src/main.c
+++ src/main.c
@@ -16,7 +16,8 @@ void help(void) {
         "  lordfetch [option]\n"
         "\n"
         "Options:\n"
-        "  -h, --help    Show this help message\n"
+        "  -h, --help       Show this help message\n"
+        "  -v, --version    Show version information\n"
         "\n"
         "Example:\n"
         "  lordfetch --help\n"
@@ -24,124 +25,167 @@ void help(void) {
 }
 
 
-void processar_flags(int argc, char *argv[]) {
-  int i = 1;
+int processar_flags(int argc, char *argv[]) {
+    int i = 1;
 
-  while (i < argc) {
-    if (strcmp(argv[i], "-h") == 0 || strcmp(argv[i], "--help") == 0) {
-      help();
-     
-      break;
-      return;
+    while (i < argc) {
+
+        if (strcmp(argv[i], "-h") == 0 ||
+            strcmp(argv[i], "--help") == 0) {
+
+            help();
+
+            return 1;
+        }
+
+        if (strcmp(argv[i], "-v") == 0 ||
+            strcmp(argv[i], "--version") == 0) {
+
+            printf("lordfetch 0.1.0\n");
+
+            return 1;
+        }
+
+        i++;
     }
-      i++;
-  }
+
+    return 0;
 }
-
-
-
-
 
 
 int main(int argc, char *argv[]) {
-  processar_flags(argc, argv);  
-  get_system_info();
-  get_distro();
-  get_cpu(); 
-  get_ram();
+
+    if (processar_flags(argc, argv))
+        return 0;
+
+    get_system_info();
+    get_distro();
+    get_cpu();
+    get_ram();
 
 
-if (is_android()) {
+    if (is_android()) {
 
-    if (is_termux()) {
-      snprintf(environment, sizeof(environment), "%s", "android \033[1;33m(termux)\033[0m");
-      show(src_logo_ascii_a_android_txt, src_logo_ascii_a_android_txt_len);
+        if (is_termux()) {
 
-    } else {
-      snprintf(environment, sizeof(environment), "%s", "android");
-      show(src_logo_ascii_a_android_txt, src_logo_ascii_a_android_txt_len);
+            snprintf(
+                environment,
+                sizeof(environment),
+                "%s",
+                "android \033[1;33m(termux)\033[0m"
+            );
+
+            show(
+                src_logo_ascii_a_android_txt,
+                src_logo_ascii_a_android_txt_len
+            );
+
+        } else {
+
+            snprintf(
+                environment,
+                sizeof(environment),
+                "%s",
+                "android"
+            );
+
+            show(
+                src_logo_ascii_a_android_txt,
+                src_logo_ascii_a_android_txt_len
+            );
+        }
+
+        return 0;
     }
+
+
+    if (strstr(distro, "fedora") != NULL) {
+
+        show(
+            src_logo_ascii_f_fedora_txt,
+            src_logo_ascii_f_fedora_txt_len
+        );
+
+        return 0;
+    }
+
+    else if (strstr(distro, "arch") != NULL) {
+
+        show(
+            src_logo_ascii_a_arch_txt,
+            src_logo_ascii_a_arch_txt_len
+        );
+
+        return 0;
+    }
+
+    else if (strstr(distro, "ubuntu") != NULL) {
+
+        show(
+            src_logo_ascii_u_ubuntu_txt,
+            src_logo_ascii_u_ubuntu_txt_len
+        );
+
+        return 0;
+    }
+
+    else if (strstr(distro, "debian") != NULL) {
+
+        show(
+            src_logo_ascii_d_debian_txt,
+            src_logo_ascii_d_debian_txt_len
+        );
+
+        return 0;
+    }
+
+    else if (strstr(distro, "mint") != NULL) {
+
+        show(
+            src_logo_ascii_l_linuxmint_txt,
+            src_logo_ascii_l_linuxmint_txt_len
+        );
+
+        return 0;
+    }
+
+    else if (strstr(distro, "pop") != NULL) {
+
+        show(
+            src_logo_ascii_p_pop_txt,
+            src_logo_ascii_p_pop_txt_len
+        );
+
+        return 0;
+    }
+
+    else if (strstr(distro, "gentoo") != NULL) {
+
+        show(
+            src_logo_ascii_g_gentoo_txt,
+            src_logo_ascii_g_gentoo_txt_len
+        );
+
+        return 0;
+    }
+
+    else if (strstr(distro, "kali") != NULL) {
+
+        show(
+            src_logo_ascii_k_kali_txt,
+            src_logo_ascii_k_kali_txt_len
+        );
+
+        return 0;
+    }
+
+    else {
+
+        printf("lordfetch: unknown distro\n");
+
+        return 0;
+    }
+
 
     return 0;
 }
- 
-
-
-
-
-
-
-
-
-
-  if (strstr(distro, "fedora") != NULL) {
-      show(src_logo_ascii_f_fedora_txt, src_logo_ascii_f_fedora_txt_len);
-      return 0;
-    }
-
-  else if (strstr(distro, "arch") != NULL) {
-      show(src_logo_ascii_a_arch_txt, src_logo_ascii_a_arch_txt_len);
-      return 0;
-    }
-
-
-
-  else if (strstr(distro, "ubuntu") != NULL) {
-      show(src_logo_ascii_u_ubuntu_txt, src_logo_ascii_u_ubuntu_txt_len);
-      return 0;
-    }
-
-
-  else if (strstr(distro, "debian") != NULL) {
-      show(src_logo_ascii_d_debian_txt, src_logo_ascii_d_debian_txt_len);
-      return 0;
-    }
-
-
-
-  else if (strstr(distro, "mint") != NULL) {
-    show(src_logo_ascii_l_linuxmint_txt, src_logo_ascii_l_linuxmint_txt_len);
-    return 0;
-  }
-
-
-
-  else if (strstr(distro, "pop") != NULL) {
-    show(src_logo_ascii_p_pop_txt, src_logo_ascii_p_pop_txt_len);
-    return 0;
-  }
-
-
-
-
-  else if (strstr(distro, "gentoo") != NULL) {
-    show(src_logo_ascii_g_gentoo_txt, src_logo_ascii_g_gentoo_txt_len);
-    return 0;
-  }
-
-  else if (strstr(distro, "kali") != NULL) {
-    show(src_logo_ascii_k_kali_txt, src_logo_ascii_k_kali_txt_len);
-    return 0;
-  }
-
-
-
-  else {
-    printf("lordfetch: unknown distro\n");
-    return 0;
-  }
-
-
-
-return 0;
-
-}
-
-
-
-
-
-
-
-
-
